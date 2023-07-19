e3aef63c0d9824fb368b3735732697d5d083f9b6
Thomas Baker <thomassb1625@gmail.com>
2023-07-18 22:54:13 -0400

    Pages setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Projects from './pages';
+import Resume from './pages';
+import Contact from './pages';
+import ReadingList from './pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1 className="pl-2 text-3xl font-bold underline">
+      Thomas Baker
+    </h1>
     <BrowserRouter>
-      <h1 className="text-3xl font-bold underline">
-        Hello hjfndjaksnfd!
-      </h1>
+      <Routes>
+        <Route path='/' element={<Projects/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/resume' element={<Resume/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/books' element={<ReadingList/>}/>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages.js
@@ -0,0 +1,54 @@
+import './css/pages.css'
+import { Link } from 'react-router-dom';
+
+export default function Projects() {
+  return (
+    <main>
+      <NavBar/>
+    </main>
+  )
+}
+
+export function Resume() {
+  return (
+    <main>
+      <NavBar/>
+    </main>
+  )
+}
+
+export function Contact() {
+  return (
+    <main>
+      <NavBar/>
+    </main>
+  )
+}
+
+export function ReadingList() {
+  return (
+    <main>
+      <NavBar/>
+    </main>
+  )
+}
+
+
+export function NavBar() {
+  return (
+    <div className='flex flex-row text-xl pl-2'>
+      <Link to='/projects' className='hover:underline'>
+        <h1>projects</h1>
+      </Link>
+      <Link to='/resume' className='pl-2 hover:underline'>
+        <h1>resume</h1>
+      </Link>
+      <Link to='/contact' className='pl-2 hover:underline'>
+        <h1>contact</h1>
+      </Link>
+      <Link to='/books' className='pl-2 hover:underline'>
+        <h1>reading list</h1>
+      </Link>
+    </div>
+  )
+}
