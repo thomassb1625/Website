2830cbb8ae026bcbef6f9a95b6f9f1118e391983
Thomas Baker <thomassb1625@gmail.com>
2023-08-22 23:13:55 -0400

    Beginnings of contact and projects page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,10 @@ const Contact = () => {
     return (
       <main>
         <NavBar/>
-        <p className='pl-2'>Contact info</p>
+        <p className='pl-2'>twitter: <a href='https://twitter.com/thomassb1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@thomassb1625</a></p>
+        <p className='pl-2'>telegram: <a href='https://t.me/Thomas_Baker1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@Thomas_Baker1625</a></p>
+        <p className='pl-2'>discord: <a href='https://discordapp.com/users/833857518111424593' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>thomassb1625</a></p>
+        <p className='pl-2'>linkedin: <a href='www.linkedin.com/in/thomas-baker-a90414168' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>Thomas Baker</a></p>
       </main>
     )
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,15 @@ const Projects = () => {
     return (
       <main>
         <NavBar/>
-        <p className='pl-2'>Project page</p>
+        <p className='pl-2 underline'>Prime protocol:</p>
+        <p className='pl-2 underline'>Cross-chain liquidation bot:</p>
+        <p className='pl-2 underline'>Arbitrage Bot:</p>
+        <p className='pl-2 underline'>Equity derivatives volatility predictor:</p>
+        <p className='pl-2 underline'>Web3 ATL:</p>
+        <p className='pl-2 underline'>Institutional DeFi Risk Report:</p>
+        <p className='pl-2 underline'>BTC gamma hedger:</p>
+        <p className='pl-2 underline'>BTC options vault:</p>
+        <p className='pl-2 underline'>Mars Lander:</p>
       </main>
     )
 };
