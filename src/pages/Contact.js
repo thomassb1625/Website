0bf3514ef841d7e429bfda1a110c4e143b5e6411
Thomas Baker <thomassb1625@gmail.com>
2023-09-24 23:32:41 -0400

    Add logos to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,10 +4,22 @@ const Contact = () => {
     return (
       <main>
         <NavBar selected='contact'/>
-        <p className='pl-2'>twitter: <a href='https://twitter.com/thomassb1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@thomassb1625</a></p>
-        <p className='pl-2'>telegram: <a href='https://t.me/Thomas_Baker1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@Thomas_Baker1625</a></p>
-        <p className='pl-2'>discord: <a href='https://discordapp.com/users/833857518111424593' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>thomassb1625</a></p>
-        <p className='pl-2'>linkedin: <a href='www.linkedin.com/in/thomas-baker-a90414168' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>Thomas Baker</a></p>
+        <p className='pl-2'>
+          <img src='/xlogo.png' alt="Twitter Logo" className="h-3 inline-block align-middle mr-2"/>
+          twitter(x?): <a href='https://twitter.com/thomassb1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@thomassb1625</a>
+        </p>
+        <p className='pl-2'>
+          <img src='/tglogo.webp' alt="Twitter Logo" className="h-3 inline-block align-middle mr-2"/>
+          telegram: <a href='https://t.me/Thomas_Baker1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@Thomas_Baker1625</a>
+        </p>
+        <p className='pl-2'>
+          <img src='/discordlogo.svg' alt="Twitter Logo" className="h-3 inline-block align-middle mr-2"/>
+          discord: <a href='https://discordapp.com/users/833857518111424593' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>thomassb1625</a>
+        </p>
+        <p className='pl-2'>
+          <img src='/linkedinlogo.png' alt="Twitter Logo" className="h-3 inline-block align-middle mr-2"/>
+          linkedin: <a href='www.linkedin.com/in/thomas-baker-a90414168' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>Thomas Baker</a>
+        </p>
       </main>
     )
 };
