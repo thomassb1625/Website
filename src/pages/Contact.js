ac86aca64f1c8bf24d8f33a2346acc6c93f0146a
Thomas Baker <thomassb1625@gmail.com>
2023-09-24 23:35:50 -0400

    Update navbar

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,15 +9,15 @@ const Contact = () => {
           twitter(x?): <a href='https://twitter.com/thomassb1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@thomassb1625</a>
         </p>
         <p className='pl-2'>
-          <img src='/tglogo.webp' alt="Twitter Logo" className="h-3 inline-block align-middle mr-2"/>
+          <img src='/tglogo.webp' alt="Telegram Logo" className="h-3 inline-block align-middle mr-2"/>
           telegram: <a href='https://t.me/Thomas_Baker1625' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>@Thomas_Baker1625</a>
         </p>
         <p className='pl-2'>
-          <img src='/discordlogo.svg' alt="Twitter Logo" className="h-3 inline-block align-middle mr-2"/>
+          <img src='/discordlogo.svg' alt="Discord Logo" className="h-3 inline-block align-middle mr-2"/>
           discord: <a href='https://discordapp.com/users/833857518111424593' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>thomassb1625</a>
         </p>
         <p className='pl-2'>
-          <img src='/linkedinlogo.png' alt="Twitter Logo" className="h-3 inline-block align-middle mr-2"/>
+          <img src='/linkedinlogo.png' alt="LinkedIn Logo" className="h-3 inline-block align-middle mr-2"/>
           linkedin: <a href='www.linkedin.com/in/thomas-baker-a90414168' rel='noreferrer' target='_blank' className='hover:underline hover:text-blue-700'>Thomas Baker</a>
         </p>
       </main>
--- src/utils/NavBar.js
+++ src/utils/NavBar.js
@@ -9,11 +9,11 @@ const NavBar = ({ selected }) => {
         <Link to='/resume' className={selected === 'resume' ? 'underline pl-2 hover:underline' : 'pl-2 hover:underline'}>
           <h1>resume</h1>
         </Link>
-        <Link to='/contact' className={selected === 'contact' ? 'underline pl-2 hover:underline' : 'pl-2 hover:underline'}>
-          <h1>contact</h1>
-        </Link>
         <Link to='/books' className={selected === 'books' ? 'underline pl-2 hover:underline' : 'pl-2 hover:underline'}>
           <h1>reading list</h1>
+        </Link>
+        <Link to='/contact' className={selected === 'contact' ? 'underline pl-2 hover:underline' : 'pl-2 hover:underline'}>
+          <h1>contact</h1>
         </Link>
       </div>
     )
