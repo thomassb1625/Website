6116cebbe9e4df87e532af1138dbdbb05b47147a
Thomas Baker <thomassb1625@gmail.com>
2023-09-24 23:23:18 -0400

    Embed notion page

--- src/pages/ReadingList.js
+++ src/pages/ReadingList.js
@@ -4,7 +4,14 @@ const ReadingList = () => {
     return (
       <main>
         <NavBar selected='books'/>
-        <p className='pl-2'>No shot I'm porting this over buy here's my Notion page</p>
+        <p className='pl-2'>No shot I'm porting this over so here's my Notion page</p>
+        <a href="https://languid-split-0ff.notion.site/Thomas-Baker-Book-Shelf-a1190a07d599476c86ada1b55c74afd1"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='hover:underline text-blue-700 pl-2'>
+          Thomas Baker Bookshelf
+          <img src="/NotionScreenshot.jpg" alt="Bookshelf Preview" className="h-56 w-96 pl-2"/>
+        </a>
       </main>
     )
 };
