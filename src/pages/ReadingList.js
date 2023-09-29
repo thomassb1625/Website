56a30042273389aaf4efa4faa1e5c6addf578bc9
Thomas Baker <thomassb1625@gmail.com>
2023-09-29 12:42:12 -0400

    Bookshelf caption

--- src/pages/ReadingList.js
+++ src/pages/ReadingList.js
@@ -4,7 +4,7 @@ const ReadingList = () => {
     return (
       <main>
         <NavBar selected='books'/>
-        <p className='pl-2'>No shot I'm porting this over so here's my Notion page</p>
+        <p className='pl-2'>Here's my notion bookshelf</p>
         <a href="https://languid-split-0ff.notion.site/Bookshelf-a1190a07d599476c86ada1b55c74afd1"
           target="_blank"
           rel="noopener noreferrer"
